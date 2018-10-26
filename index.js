20d68b49ca4d8728bb8f03f706e1359ddb29107f
Александра <>
2018-10-26 19:04:10 +0500

    used TypeScript, changed the file reading to asynchronous, removed the unused code and comments, used the linter

--- index.js
+++ index.js
@@ -1,58 +1,64 @@
-define(["require", "exports", "express", "fs"], function (require, exports, express, fs) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    var app = express();
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-    var VALID_EVENT_TYPES = ['critical', 'info'];
-    var time = new Date();
-    var startTime = time.getTime();
-    var format = function (time) {
-        time = Math.floor(time / 1000);
-        var hh = Math.floor(time / 3600).toString().padStart(2, '0');
-        var mm = Math.floor((time % 3600) / 60).toString().padStart(2, '0');
-        var ss = ((time % 3600) % 60).toString().padStart(2, '0');
-        return hh + ":" + mm + ":" + ss;
-    };
-    app.get('/status', function (_, res) {
-        var workTime = new Date().getTime() - startTime;
-        res.send(format(workTime));
-    });
-    var sendEvents = function (res, type, page, size) {
-        var input = JSON.parse(fs.readFileSync('events.json', 'utf8'));
-        page = page || 1;
-        size = size || input.events.length;
-        if (type) {
-            var t_1 = type.split(':');
-            if (t_1.some(function (e) { return VALID_EVENT_TYPES.indexOf(e) == -1; })) {
-                res.status(400).send("incorrect type");
-                return;
-            }
-            else {
-                input.events = input.events.filter(function (event) { return t_1.indexOf(event.type) != -1; });
-            }
-        }
-        var startIndex = (page - 1) * size;
-        var endIndex = Math.min(input.events.length, page * size);
-        var events = [];
-        for (var i = startIndex; i < endIndex; i++) {
-            events.push(input.events[i]);
-        }
-        input.events = events;
-        res.send(input);
-    };
-    app.get('/api/events', function (req, res) {
-        var type = req.query.type;
-        var page = req.query.page;
-        var size = req.query.size;
-        sendEvents(res, type, page, size);
-    });
-    app.post('/api/events', function (req, res) {
-        var type = req.body.type;
-        var page = req.body.page;
-        var size = req.body.size;
-        sendEvents(res, type, page, size);
-    });
-    app.get('*', function (_, res) { return res.status(404).send("<h1>Page not found</h1>"); });
-    app.listen(8000);
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const fs_1 = __importDefault(require("fs"));
+const app = express_1.default();
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+const VALID_EVENT_TYPES = ['critical', 'info'];
+let time = new Date();
+let startTime = time.getTime();
+const format = (time) => {
+    time = Math.floor(time / 1000);
+    const hh = Math.floor(time / 3600)
+        .toString()
+        .padStart(2, '0');
+    const mm = Math.floor((time % 3600) / 60)
+        .toString()
+        .padStart(2, '0');
+    const ss = ((time % 3600) % 60).toString().padStart(2, '0');
+    return `${hh}:${mm}:${ss}`;
+};
+app.get('/status', (_, res) => {
+    const workTime = new Date().getTime() - startTime;
+    res.send(format(workTime));
 });
+const sendEvents = (res, type, page, size) => {
+    const input = JSON.parse(fs_1.default.readFileSync('events.json', 'utf8'));
+    page = page || 1;
+    size = size || input.events.length;
+    if (type) {
+        const t = type.split(':');
+        if (t.some(e => !VALID_EVENT_TYPES.includes(e))) {
+            return res.status(400).send('incorrect type');
+        }
+        else {
+            input.events = input.events.filter((event) => t.indexOf(event.type) != -1);
+        }
+    }
+    const startIndex = (page - 1) * size;
+    const endIndex = Math.min(input.events.length, page * size);
+    let events = [];
+    for (let i = startIndex; i < endIndex; i++) {
+        events.push(input.events[i]);
+    }
+    input.events = events;
+    return res.send(input);
+};
+app.get('/api/events', (req, res) => {
+    const type = req.query.type;
+    const page = req.query.page;
+    const size = req.query.size;
+    sendEvents(res, type, page, size);
+});
+app.post('/api/events', (req, res) => {
+    const type = req.body.type;
+    const page = req.body.page;
+    const size = req.body.size;
+    sendEvents(res, type, page, size);
+});
+app.get('*', (_, res) => res.status(404).send('<h1>Page not found</h1>'));
+app.listen(8000);
--- index.ts
+++ index.ts
@@ -1,10 +1,9 @@
-import express = require('express');
+import express from 'express';
+import fs from 'fs';
 const app = express();
-import fs = require('fs');
 
-app.use(express.json()); 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 
 const VALID_EVENT_TYPES = ['critical', 'info'];
 
@@ -13,49 +12,61 @@ let startTime = time.getTime();
 
 const format = (time: number) => {
 	time = Math.floor(time / 1000);
-	const hh = Math.floor(time / 3600).toString().padStart(2, '0');
-	const mm = Math.floor((time % 3600) / 60).toString().padStart(2, '0');
+	const hh = Math.floor(time / 3600)
+		.toString()
+		.padStart(2, '0');
+	const mm = Math.floor((time % 3600) / 60)
+		.toString()
+		.padStart(2, '0');
 	const ss = ((time % 3600) % 60).toString().padStart(2, '0');
 	return `${hh}:${mm}:${ss}`;
 };
-
 
 app.get('/status', (_, res) => {
 	const workTime = new Date().getTime() - startTime;
 	res.send(format(workTime));
 });
 
-const sendEvents = (res: Response, type: string, page: number, size: number) => {
-	const input = JSON.parse(fs.readFileSync('events.json', 'utf8'));
-
-	page = page || 1;
-	size = size || input.events.length;
-
-	if(type) {
-		const t = type.split(':');
-
-		if(t.some(e => VALID_EVENT_TYPES.indexOf(e) == -1)) {
-			res.status(400).send("incorrect type");
-			return;
-		}
-		else {
-			input.events = input.events.filter(event => t.indexOf(event.type) != -1);
-		}
-	}
-
-	const startIndex = (page - 1) * size;
-	const endIndex = Math.min(input.events.length, page * size);
-	let events = [];
-
-	for(let i = startIndex; i < endIndex; i++) {
-		events.push(input.events[i]); 
-	}	
-	input.events = events;
-
-	res.send(input);
+interface Event {
+	type: string;
 }
 
+const sendEvents = (
+	res: express.Response,
+	type: string,
+	page: number,
+	size: number
+) => {
+	fs.readFile('events.json', 'utf8', (_, data) => {
+		const input = JSON.parse(data);
 
+		page = page || 1;
+		size = size || input.events.length;
+
+		if (type) {
+			const t = type.split(':');
+
+			if (t.some(e => !VALID_EVENT_TYPES.includes(e))) {
+				return res.status(400).send('incorrect type');
+			} else {
+				input.events = input.events.filter(
+					(event: Event) => t.indexOf(event.type) != -1
+				);
+			}
+		}
+
+		const startIndex = (page - 1) * size;
+		const endIndex = Math.min(input.events.length, page * size);
+		let events = [];
+
+		for (let i = startIndex; i < endIndex; i++) {
+			events.push(input.events[i]);
+		}
+		input.events = events;
+
+		return res.send(input);
+	});	
+};
 
 app.get('/api/events', (req, res) => {
 	const type = req.query.type;
@@ -64,16 +75,13 @@ app.get('/api/events', (req, res) => {
 	sendEvents(res, type, page, size);
 });
 
-app.post('/api/events', (req,res) => {
+app.post('/api/events', (req, res) => {
 	const type = req.body.type;
 	const page = req.body.page;
 	const size = req.body.size;
 	sendEvents(res, type, page, size);
 });
 
-app.get('*', (_, res) => res.status(404).send("<h1>Page not found</h1>"));
-
+app.get('*', (_, res) => res.status(404).send('<h1>Page not found</h1>'));
 
 app.listen(8000);
-
-
